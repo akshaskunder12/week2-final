176b92c71c2f23b01e68226311e7b42768944ff2
Aksha S Kunder <147728118+akshaskunder12@users.noreply.github.com>
2023-11-04 21:44:29 +0530

    Add files via upload

---
+++ search.jsx
@@ -0,0 +1,16 @@
+
+export const searchData = [
+    { keyword: 'gokarna', route: './Places' },
+    
+];
+
+const generateRandomSearchData = (keyword) => {
+    return searchData.find((item) => item.keyword === keyword.toLowerCase());
+};
+
+const Search={
+    searchData,
+    generateRandomSearchData,
+};
+
+  export default Search;
